610e571f75829ba486856f5e445dd4e6f65bff5b
Lauren Ferreira <lauren.ferreira@fox.com>
2021-12-15 16:11:00 -0300

    [Day 9] Incomplete solution to the second challenge

--- 2021/09/main.go
+++ 2021/09/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -28,22 +29,32 @@ func main() {
 func solve1(input []string) (result int) {
 	parsedInput := parseInput(input)
 
-	var lowests []int
-	for i := 0; i < len(parsedInput[0]); i++ {
-		for j := 0; j < len(parsedInput); j++ {
+	lowests := findLowests(parsedInput)
+
+	risk := 0
+	for _, lowest := range lowests {
+		risk += (lowest[0] + 1)
+	}
+	return risk
+}
+
+func findLowests(input [][]int) [][3]int {
+	var positions [][3]int // [ value ] [ i ] [ j ]
+	for i := 0; i < len(input[0]); i++ {
+		for j := 0; j < len(input); j++ {
 			var adjacents []int
-			value := parsedInput[j][i]
+			value := input[j][i]
 			if i-1 >= 0 {
-				adjacents = append(adjacents, parsedInput[j][i-1])
+				adjacents = append(adjacents, input[j][i-1])
 			}
-			if i+1 < len(parsedInput[0]) {
-				adjacents = append(adjacents, parsedInput[j][i+1])
+			if i+1 < len(input[0]) {
+				adjacents = append(adjacents, input[j][i+1])
 			}
 			if j-1 >= 0 {
-				adjacents = append(adjacents, parsedInput[j-1][i])
+				adjacents = append(adjacents, input[j-1][i])
 			}
-			if j+1 < len(parsedInput) {
-				adjacents = append(adjacents, parsedInput[j+1][i])
+			if j+1 < len(input) {
+				adjacents = append(adjacents, input[j+1][i])
 			}
 			isLower := true
 			for _, adjacent := range adjacents {
@@ -53,21 +64,69 @@ func solve1(input []string) (result int) {
 				}
 			}
 			if isLower {
-				lowests = append(lowests, value)
-				fmt.Println(value, adjacents)
+				positions = append(positions, [3]int{value, i, j})
 			}
 		}
 	}
-
-	risk := 0
-	for _, lowest := range lowests {
-		risk += (lowest + 1)
-	}
-	return risk
+	return positions
 }
 
 func solve2(input []string) (result int) {
-	return 0
+	parsedInput := parseInput(input)
+
+	lowests := findLowests(parsedInput)
+
+	currentLowests := [3]int{9999, 9, 9}
+	currentLowest := lowests[0][0]
+	for _, lowest := range lowests {
+		basin := findBasin(parsedInput, lowest)
+		if currentLowest <= basin {
+			continue
+		}
+		currentLowest = addLowests(currentLowests[:], basin)
+	}
+
+	mult := 1
+	for _, lowest := range currentLowests {
+		mult *= lowest
+	}
+
+	return mult
+}
+
+func findBasin(input [][]int, lowest [3]int) (basin int) {
+	return countBasin(input, lowest[1], lowest[2], 0)
+}
+
+func countBasin(input [][]int, i, j, basin int) int {
+	basin++
+	if j-1 >= 0 {
+		basin = countBasin(input, i, j-1, basin)
+	}
+	if j+1 < len(input[0])-1 {
+		basin = countBasin(input, i, j+1, basin)
+	}
+	if i-1 > 0 {
+		basin = countBasin(input, i-1, j, basin)
+	}
+	if i+1 < len(input)-1 {
+		basin = countBasin(input, i+1, j, basin)
+	}
+	return basin
+}
+
+func addLowests(currentLowests []int, input int) (lowest int) {
+	sort.Ints(currentLowests)
+
+	for i, lowest := range currentLowests {
+		if input >= lowest {
+			continue
+		}
+		currentLowests[i] = input
+		break
+	}
+
+	return currentLowests[0]
 }
 
 func parseInput(input []string) [][]int {
--- 2021/09/main_test.go
+++ 2021/09/main_test.go
@@ -48,9 +48,15 @@ func Test_solve2(t *testing.T) {
 		{
 			name: "test1",
 			args: args{
-				input: []string{},
+				input: []string{
+					"2199943210",
+					"3987894921",
+					"9856789892",
+					"8767896789",
+					"9899965678",
+				},
 			},
-			wantResult: 0,
+			wantResult: 1134,
 		},
 	}
 	for _, tt := range tests {
@@ -73,6 +79,118 @@ func Test_main(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			main()
+		})
+	}
+}
+
+func Test_addLowests(t *testing.T) {
+	type args struct {
+		inputs []int
+		input  int
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantLowest int
+	}{
+		{
+			name: "test1",
+			args: args{
+				inputs: []int{4, 2, 6},
+				input:  3,
+			},
+			wantLowest: 2,
+		},
+		{
+			name: "test2",
+			args: args{
+				inputs: []int{2, 4, 6},
+				input:  1,
+			},
+			wantLowest: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotLowest := addLowests(tt.args.inputs, tt.args.input); gotLowest != tt.wantLowest {
+				t.Errorf("addLowests() = %v, want %v", gotLowest, tt.wantLowest)
+			}
+		})
+	}
+}
+
+func Test_findBasin(t *testing.T) {
+	type args struct {
+		input   [][]int
+		lowests [3]int
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantBasin int
+	}{
+		{
+			name: "test1",
+			args: args{
+				input: [][]int{
+					{2, 1, 9, 9, 9, 4, 3, 2, 1, 0},
+					{3, 9, 8, 7, 8, 9, 4, 9, 2, 1},
+					{9, 8, 5, 6, 7, 8, 9, 8, 9, 2},
+					{8, 7, 6, 7, 8, 9, 6, 7, 8, 9},
+					{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
+				},
+				lowests: [3]int{2, 0, 0},
+			},
+			wantBasin: 3,
+		},
+		{
+			name: "test2",
+			args: args{
+				input: [][]int{
+					{2, 1, 9, 9, 9, 4, 3, 2, 1, 0},
+					{3, 9, 8, 7, 8, 9, 4, 9, 2, 1},
+					{9, 8, 5, 6, 7, 8, 9, 8, 9, 2},
+					{8, 7, 6, 7, 8, 9, 6, 7, 8, 9},
+					{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
+				},
+				lowests: [3]int{0, 9, 0},
+			},
+			wantBasin: 9,
+		},
+		{
+			name: "test3",
+			args: args{
+				input: [][]int{
+					{2, 1, 9, 9, 9, 4, 3, 2, 1, 0},
+					{3, 9, 8, 7, 8, 9, 4, 9, 2, 1},
+					{9, 8, 5, 6, 7, 8, 9, 8, 9, 2},
+					{8, 7, 6, 7, 8, 9, 6, 7, 8, 9},
+					{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
+				},
+				lowests: [3]int{5, 2, 2},
+			},
+			wantBasin: 14,
+		},
+		{
+			name: "test4",
+			args: args{
+				input: [][]int{
+					{2, 1, 9, 9, 9, 4, 3, 2, 1, 0},
+					{3, 9, 8, 7, 8, 9, 4, 9, 2, 1},
+					{9, 8, 5, 6, 7, 8, 9, 8, 9, 2},
+					{8, 7, 6, 7, 8, 9, 6, 7, 8, 9},
+					{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
+				},
+				lowests: [3]int{5, 6, 4},
+			},
+			wantBasin: 9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotBasin := findBasin(tt.args.input, tt.args.lowests); gotBasin != tt.wantBasin {
+				t.Errorf("findBasin() = %v, want %v", gotBasin, tt.wantBasin)
+			}
 		})
 	}
 }
